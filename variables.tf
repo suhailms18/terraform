381c59881be9aaff1e2a2ecc0d935c6c3f6b7e27
Muhammed Suhail <142370111+suhailms18@users.noreply.github.com>
2024-01-18 17:58:36 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -7,6 +7,14 @@ variable "aws" {
   }
 }
 
+variable "secret_key" {
+  default = "AKIATZETYSL53GMK6KF5"
+}
+
+variable "access_key" {
+  default = "8yVYs1IKdscmvRnZc3obclWTTyBPtsNDb6SUK5nu"
+} 
+
 variable "account_alias" {
   description = "AWS IAM account alias for this project"
   type        = string
