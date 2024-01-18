56c70d1ad732c3098c10daf1550d0f35d7557a20
Muhammed Suhail <142370111+suhailms18@users.noreply.github.com>
2024-01-18 18:00:41 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,6 +1,8 @@
 provider "aws" {
-  region  = var.aws["region"]
-  profile = var.aws["profile"]
+  region     = var.aws["region"]
+  #profile   = var.aws["profile"]
+  access_key = var.access_key
+  secret_key = var.secret_key
 }
 
 data "aws_caller_identity" "current" {}
